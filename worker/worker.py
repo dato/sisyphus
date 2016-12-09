dbf1e65160661229b20f14fb9908290adf09a444
Adeodato Simó <dato@fi.uba.ar>
2016-12-09 17:18:55 -0300

    Move out "skel" logic into the worker; make worker more modular.

--- worker/worker.py
+++ worker/worker.py
@@ -1,24 +1,26 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.6
 
-"""Worker para el corrector automático de Algoritmos II.
+"""Worker para el corrector automático.
 
-El workflow es simple a propósito:
+El script:
 
-  - recibe un archivo tar por entrada estándar y lo desempaca en un directorio
-    temporal
+  • recibe por entrada estándar un archivo en formato TAR con todos los
+    archivos de la corrección:
 
-  - ejecuta `make` en el directorio temporal, con un timeout
+      ⁃ "orig": el código del alumno, tal y como se recibió
+      ⁃ "skel": el código base del TP, incluyendo pruebas públicas
+      ⁃ "priv": pruebas privadas al corrector
 
-Así, el worker necesita muy poca lógica y se puede ejecutar en un sistema
-de sólo lectura.
+  • ejecuta el corrector especificado con --corrector e imprime el
+    resultado por salida estándar.
 
-Nota: si se llega al timeout, la función ejecutar() no mata a los procesos
-hijos. Quien llame al worker debería encargarse, por ejemplo ejecutando al
-worker dentro de Docker.
+  • si hubo errores de ejecución (no de corrección), termina con estado de
+    salida distinto de cero.
 """
 
 import argparse
 import atexit
+import pathlib
 import shutil
 import signal
 import subprocess
@@ -27,7 +29,47 @@ import tarfile
 import tempfile
 
 
-def ejecutar(timeout):
+class ErrorAlumno(Exception):
+  pass
+
+
+class CorregirV2:
+  """Corrector compatible con la versión 2.
+
+  Sobreescribe en la entrega del alumno los archivos del esqueleto,
+  e invoca a make.
+  """
+  def __init__(self, path):
+    path = pathlib.Path(path)
+    orig = path / "orig"
+    skel = path / "skel"
+
+    badmake = {"makefile", "GNUmakefile"}.intersection(orig.iterdir())
+
+    if badmake:
+      name = badmake.pop()
+      raise ErrorAlumno(f"archivo ‘{name}’ no aceptado; solo ‘Makefile’")
+
+    if skel.is_dir():
+      for file in skel.iterdir():
+        shutil.copy2(file, orig)
+
+    self.cwd = orig
+
+  def run(self):
+    cmd = subprocess.run(["make"], cwd=self.cwd, stdin=subprocess.DEVNULL,
+                         stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+    print("Todo OK" if cmd.returncode == 0 else "ERROR",
+          cmd.stdout.decode("utf-8", "replace"), sep="\n\n", end="")
+
+
+CORRECTORES = {
+    "v2": CorregirV2,
+}
+
+
+def ejecutar(corrector, timeout):
   """Función principal del script.
   """
   tmpdir = tempfile.mkdtemp(prefix="corrector.")
@@ -35,20 +77,14 @@ def ejecutar(timeout):
 
   # Usamos sys.stdin.buffer para leer en binario (sys.stdin es texto). Asimismo,
   # el modo ‘r|’ (en lugar de ‘r’) indica que fileobj no es seekable.
-  try:
-    with tarfile.open(fileobj=sys.stdin.buffer, mode="r|") as tar:
-      tar.extractall(tmpdir)
-  except tarfile.TarError as ex:
-    sys.stderr.write("Error al desempacar: {}\n".format(ex))
-    return -1
+  with tarfile.open(fileobj=sys.stdin.buffer, mode="r|") as tar:
+    tar.extractall(tmpdir)
 
   signal.alarm(timeout)
   try:
-    return subprocess.call(["make"], cwd=tmpdir,
-                           stdin=subprocess.DEVNULL)
+    corrector(tmpdir).run()
   except Timeout:
-    sys.stderr.write("El proceso tardó más de {} segundos\n".format(timeout))
-    return 3
+    raise ErrorAlumno("El proceso tardó más de {} segundos".format(timeout))
   finally:
     signal.alarm(0)
 
@@ -59,9 +95,14 @@ def main():
   parser.add_argument("--timeout", type=int, default=120,
                       help="tiempo máximo de ejecución en segundos")
 
-  args = parser.parse_args()
+  parser.add_argument("--corrector", type=str, choices=list(CORRECTORES),
+                      help="corrector (lenguaje) a usar", default="v2")
 
-  return ejecutar(args.timeout)
+  args = parser.parse_args()
+  try:
+    ejecutar(CORRECTORES[args.corrector], args.timeout)
+  except ErrorAlumno as ex:
+    print("ERROR: {}.".format(ex))
 
 ##
 
@@ -79,6 +120,6 @@ signal.signal(signal.SIGALRM, raise_timeout)
 ##
 
 if __name__ == "__main__":
-  sys.exit(main())
+  main()
 
 # vi:et:sw=2
