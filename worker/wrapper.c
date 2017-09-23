34e94afa5a499b92dbfe529c6cb344d0fddedbde
Adeodato Simó <dato@fi.uba.ar>
2017-09-23 16:57:04 -0300

    wrapper.c: allow DOCKER_IMAGE to be defined from the command line.

--- worker/wrapper.c
+++ worker/wrapper.c
@@ -8,7 +8,10 @@
 #include <string.h>
 #include <unistd.h>
 
+#ifndef DOCKER_IMAGE
 #define DOCKER_IMAGE "algoritmosrw/corrector"
+#endif
+
 #define MEM_LIM "512m"
 
 /* El wrapper pasa los argumentos que recibe al worker. Por eso esta lista
