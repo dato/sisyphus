a655d57a9a6d03cea5165a87d64e37ae65154178
Adeodato Simó <dato@fi.uba.ar>
2020-06-16 14:24:32 -0300

    yamltap: soporte para variables de entorno, y regex matching.

--- sisyphus/tools/yamltap.py
+++ sisyphus/tools/yamltap.py
@@ -12,6 +12,8 @@ con el script pruebas.sh.
 import argparse
 import enum
 import subprocess
+import os
+import re
 import sys
 import textwrap
 
@@ -20,6 +22,17 @@ from typing import Dict, List, Optional
 import yaml
 
 from pydantic import BaseModel, Field, ValidationError
+
+
+class Env(str, enum.Enum):
+    EXTEND = "extend"
+    REPLACE = "replace"
+
+
+class Match(str, enum.Enum):
+    LITERAL = "literal"
+    MULTI_REGEX = "regex"
+    SINGLE_REGEX = "multiline_regex"
 
 
 class Result(enum.Enum):
@@ -35,9 +48,14 @@ class Test(BaseModel):
     stdin: Optional[str]
     # Expected return code
     retcode: int = 0
-    # These, if present, must match exactly.
+    # These, if present, must match in the specified match policy.
     stdout: Optional[str]
     stderr: Optional[str]
+    stdout_policy: Match = Match.LITERAL
+    stderr_policy: Match = Match.LITERAL
+    # Environment variables (added to the existing environment, or replacing it)
+    env: Optional[Dict[str, str]]
+    env_policy: Env = Env.EXTEND
 
     class Config:
         extra = "forbid"
@@ -152,10 +170,20 @@ def run_test(test):
     Returns:
       una tupla (result, report_dict).
     """
+    proc_env = None
     proc_stdin = None if test.stdin is not None else subprocess.DEVNULL
+
+    if test.env is not None:
+        if test.env_policy == Env.REPLACE:
+            proc_env = test.env
+        else:
+            assert test.env_policy == Env.EXTEND
+            proc_env = os.environ.copy()
+            proc_env.update(test.env)
 
     proc = subprocess.run(
         [test.program] + test.args,
+        env=proc_env,
         text=True,
         input=test.stdin,
         stdin=proc_stdin,
@@ -167,11 +195,15 @@ def run_test(test):
     if proc.returncode != test.retcode:
         report["retcode"] = f"retcode={proc.returncode}, se esperaba {test.retcode}"
 
-    # TODO: diff de stderr
     # TODO: diff línea a línea, como en csvdiff.py
 
-    if proc.stdout != test.stdout:
-        report["stdout"] = f"stdout={proc.stdout}, se esperaba {test.stdout}"
+    if test.stdout is not None:
+        if result := report_diff(test.stdout, proc.stdout, test.stdout_policy):
+            report["stdout"] = result
+
+    if test.stderr is not None:
+        if result := report_diff(test.stderr, proc.stderr, test.stderr_policy):
+            report["stderr"] = result
 
     return (Result.FAIL if report else Result.OK, report)
 
@@ -197,6 +229,19 @@ def run_tests(tests, *, offset=0):
 
     if offset > 0:
         print(f"1..{len(tests) + offset}")
+
+
+def report_diff(expected: str, actual: str, policy: Match):
+    if policy == Match.LITERAL:
+        if expected != actual:
+            # TODO: better reporting
+            return f"{actual}, se esperaba {expected}"
+
+    if policy == Match.SINGLE_REGEX:
+        regex = re.compile(expected, re.M)
+        if not regex.search(actual):
+            return f"{actual}\nno contiene regex\n{regex.pattern}"
+
 
 if __name__ == "__main__":
     sys.exit(main())
