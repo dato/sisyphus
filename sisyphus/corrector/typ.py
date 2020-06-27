3b2f7a029fc6371c7628c6a90582d758e74097a9
Adeodato Simó <dato@fi.uba.ar>
2020-06-27 01:54:40 -0300

    Crear defaults de checks si no se especifican explícitamente

--- sisyphus/corrector/typ.py
+++ sisyphus/corrector/typ.py
@@ -1,3 +1,4 @@
+from dataclasses import field
 from pathlib import Path
 from typing import Dict, List, Optional
 
@@ -42,12 +43,22 @@ class Materia:
     """Clase que representa la configuración de una materia.
     """
 
-    checks: Dict[str, Check]
     entregas: Dict[str, Entrega]
+    checks: Dict[str, Check] = field(default_factory=dict)
+
+    @validator("checks", pre=True)
+    def checkrun_names(cls, checks):
+        """Si un check no incluye nombre, se le da uno por omisión.
+        """
+        for key, check in checks.items():
+            check.setdefault("name", f"Pruebas {key}")
+        return checks
 
     @validator("entregas", pre=True)
     def entrega_defaults(cls, entregas):
-        """Se usa el nombre de la entrega como default para rama, checks y alu_dir.
+        """Se usa el nombre de la entrega como default para rama y alu_dir.
+
+        Si no se especifica ningún check, se creará uno default.
         """
         if isinstance(entregas, list):
             # Por conveniencia, permitimos que las entregas sean una lista
@@ -58,3 +69,11 @@ class Materia:
             attrs.setdefault("alu_dir", entrega)
             attrs.setdefault("checks", [entrega])
         return entregas
+
+    def __post_init_post_parse__(self):
+        """Rellena checks faltantes con un check default.
+        """
+        for entrega in self.entregas.values():
+            for check in entrega.checks:
+                if check not in self.checks:
+                    self.checks[check] = Check(name=f"Pruebas {check}")
